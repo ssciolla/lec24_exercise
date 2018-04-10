0ff15f45f9c79bac512de930377da514e18ee1e2
ssciolla <samgsciolla@gmail.com>
2018-04-10 14:42:03 -0400

    Michigan Football Stats

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request
+import model
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '''
+        <img src="/static/blockM.png"/>
+        <h1>Michigan Sports Info!</h1>
+        <ul>
+            <li><a href="/fball"> Men's Football </a></li>
+        </ul>
+    '''
+
+@app.route('/fball', methods=['GET', 'POST'])
+def fball():
+    if request.method == 'POST':
+        sortby = request.form['sortby']
+        sortorder = request.form['sortorder']
+        seasons = model.get_fball_seasons(sortby, sortorder)
+    else:
+        seasons = model.get_fball_seasons()
+    return render_template("seasons.html", seasons=seasons)
+
+@app.route('/hello', methods=['GET', 'POST'])
+def hello():
+    if request.method == 'POST':
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+    else:
+        firstname = ''
+        lastname = ''
+    return render_template("hello.html", firstname=firstname, lastname=lastname)
+
+if __name__ == '__main__':
+    model.init_fball()
+    app.run(debug=True)
---
+++ model.py
@@ -0,0 +1,33 @@
+#model.py
+import csv
+
+FB_FILE_NAME = 'umfootball.csv'
+
+fb_seasons = []
+
+def init_fball(csv_file_name=FB_FILE_NAME):
+    with open(csv_file_name) as f:
+        reader = csv.reader(f)
+        next(reader) # throw away headers
+        next(reader) # throw away headers
+        global fb_seasons
+        fb_seasons = [] # reset, start clean
+        for r in reader:
+            r[3] = int(r[3])
+            r[4] = int(r[4])
+            r[5] = float(r[6]) * 100
+            fb_seasons.append(r)
+
+def get_fball_seasons(sortby='year', sortorder='desc'):
+    global fb_seasons
+    if sortby == 'year':
+        sortcol = 1
+    elif sortby == 'wins':
+        sortcol = 3
+    elif sortby == 'pct':
+        sortcol = 5
+    else:
+        sortcol = 0
+    rev = (sortorder == 'desc')
+    sorted_list = sorted(fb_seasons, key=lambda row: row[sortcol], reverse=rev)
+    return sorted_list
